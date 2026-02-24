cd0346ac25fd2ee0632c7242e197ac3cf2a59a20
Monty <montather.al-hassy@mohawkcollege.ca>
2026-02-23 22:59:19 -0500

    Add
    - Function to draw eyes
    - Function to draw stars
    - Star x,y coords

--- Al-Hassy-Monty-a2-Interactive-Drawing/Game.cs
+++ Al-Hassy-Monty-a2-Interactive-Drawing/Game.cs
@@ -1,5 +1,6 @@
 ﻿// Include the namespaces (code libraries) you need below.
 using System;
+using System.Diagnostics.Metrics;
 using System.Numerics;
 
 // The namespace your code is in.
@@ -12,20 +13,30 @@ namespace MohawkGame2D
     {
         // Place your variables here:
 
-            // Shades colors
-        Color purple = new Color(128, 0, 128);
-        Color pink = new Color(255, 192, 203);
-        Color[] shadesColors = [Color.Black, Color.Red, Color.Cyan, Color.Yellow, purple, pink];
+        // Shades colors (New colors = Purple, Pink)
+        Color[] shadesColors = [Color.Black, Color.Red, Color.Cyan, Color.Green, Color.Yellow, 
+                                new Color(128, 0, 128), new Color(255, 192, 203)];
 
 
+        // Remaining facial feature colors
+
+        // Eyebrow
+        Color brown = new Color(150, 75, 0);
+
+        // Eye colors (Maroon, Turqoise, Orange)
+        Color[] eyeColors = [new Color(128, 0, 0), new Color(64, 224, 208), new Color(255, 165, 0)];
+
+        // Random locations for stars
+        int randomXCoords = Random.Integer(30, 770);
+        int randomYCoords = Random.Integer(30, 370);
 
         /// <summary>
         ///     Setup runs once before the game loop begins.
         /// </summary>
         public void Setup()
         {
+            Window.SetTitle("Shades up, stomach out.");
             Window.SetSize(800, 600);
-            Window.SetTitle("Shades up, stomach out.")
         }
 
         /// <summary>
@@ -33,15 +44,56 @@ namespace MohawkGame2D
         /// </summary>
         public void Update()
         {
+            Window.ClearBackground(Color.White);
 
+            if (Input.IsMouseButtonDown(MouseInput.Left)) 
+            {
+
+            }
+            else
+            {
+                Draw.FillColor = shadesColors[0];
+                drawShades(320, 220);
+                Draw.FillColor = Color.Black;
+                Draw.Rectangle(200, 500, 400, 50);
+            }
         }
 
         void drawShades(int x, int y)
         {
-            Draw.Circle(x - 150, y, 100);
-            Draw.Rectangle(x, y, 120, 60);
-            Draw.Circle(x + 150, y, 100);
+            Draw.Rectangle(x, y, 160, 40);
+            Draw.Circle(x - 70, y - 2, 100);
+            Draw.Circle(x + 210, y - 2, 100);
         }
+        void drawEyeBrow(int x, int y)
+        {
+            Draw.FillColor = (brown);
+
+        }
+        void drawEye(int x, int y)
+        {
+              // Draw eye shape
+            Draw.LineSize = 3;
+            Draw.LineColor = (Color.Black);
+            Draw.FillColor = (Color.White);
+            Draw.Square(x, y, 100);
+
+            // Draw inner eye
+            Draw.FillColor = eyeColors[0];
+            Draw.Circle(x, y, 100);
+            Draw.FillColor = eyeColors[1];
+            Draw.Circle(x, y, 75);
+            Draw.FillColor = eyeColors[2];
+            Draw.Circle(x, y, 50);
+        }
+
+        void drawStars(int x, int y)
+        {
+            Draw.FillColor = Color.Yellow;
+            Draw.Ellipse(x, y, 10, 35);
+            Draw.Ellipse(x, y, 35, 10);
+        }
+
     }
 
 }
