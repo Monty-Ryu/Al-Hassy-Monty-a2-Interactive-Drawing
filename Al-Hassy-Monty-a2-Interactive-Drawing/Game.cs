f01845714d8534ba2122bae7b115f7dfb7241031
Monty <montather.al-hassy@mohawkcollege.ca>
2026-02-23 21:05:06 -0500

    Add
    - Shades color array
    - Shades draw function
    - Window size and name

---
+++ Al-Hassy-Monty-a2-Interactive-Drawing/Game.cs
@@ -0,0 +1,47 @@
+﻿// Include the namespaces (code libraries) you need below.
+using System;
+using System.Numerics;
+
+// The namespace your code is in.
+namespace MohawkGame2D
+{
+    /// <summary>
+    ///     Your game code goes inside this class!
+    /// </summary>
+    public class Game
+    {
+        // Place your variables here:
+
+            // Shades colors
+        Color purple = new Color(128, 0, 128);
+        Color pink = new Color(255, 192, 203);
+        Color[] shadesColors = [Color.Black, Color.Red, Color.Cyan, Color.Yellow, purple, pink];
+
+
+
+        /// <summary>
+        ///     Setup runs once before the game loop begins.
+        /// </summary>
+        public void Setup()
+        {
+            Window.SetSize(800, 600);
+            Window.SetTitle("Shades up, stomach out.")
+        }
+
+        /// <summary>
+        ///     Update runs every frame.
+        /// </summary>
+        public void Update()
+        {
+
+        }
+
+        void drawShades(int x, int y)
+        {
+            Draw.Circle(x - 150, y, 100);
+            Draw.Rectangle(x, y, 120, 60);
+            Draw.Circle(x + 150, y, 100);
+        }
+    }
+
+}
